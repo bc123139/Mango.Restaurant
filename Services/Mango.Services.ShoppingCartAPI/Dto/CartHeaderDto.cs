07a97d3b6b28de152d5034fa99dafef5a3fc810d
Usman Tahir <Usman.tahir@synergy-it.com>
2022-01-06 23:29:46 +0500

    Add ShoppingCart project in solution

---
+++ Services/Mango.Services.ShoppingCartAPI/Dto/CartHeaderDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ShoppingCartAPI.Dto
+{
+    public class CartHeaderDto
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string CouponCode { get; set; }
+    }
+}
