aab80f7050e75588b43c0d825bfe4abbcde3c1a0
Usman Tahir <Usman.tahir@synergy-it.com>
2022-01-14 01:15:25 +0500

    Add OrderAPI in solution

---
+++ Services/Mango.Services.OrderAPI/Database/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Services.OrderAPI.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.OrderAPI.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+        public DbSet<OrderHeader> OrderHeader { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+    }
+}
---
+++ Services/Mango.Services.OrderAPI/Database/Entities/OrderDetails.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Mango.Services.OrderAPI.Database.Entities
+{
+    public class OrderDetails
+    {
+        public int Id { get; set; }
+        public int OrderHeaderId { get; set; }
+        [ForeignKey(nameof(OrderHeaderId))]
+        public virtual OrderHeader OrderHeader { get; set; }
+        public int ProductId { get; set; }
+
+        public int Count { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+    }
+}
